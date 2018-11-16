704be474ce6b80e946c517b2281985c424ca0f7b
Administrator <Administrator@WIN-J3EQULD7DG7>
2018-11-16 18:07:47 +0800

    20181116

--- src/main/java/com/machintosh1983/var/platform/controller/PlatformFrontController.java
+++ src/main/java/com/machintosh1983/var/platform/controller/PlatformFrontController.java
@@ -6,6 +6,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
